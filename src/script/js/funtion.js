a86038b5512b48eeea5c6937df5a9d6c56ef6e40
“ <1076968705@qq.com>
2019-07-08 21:49:38 +0800

    星期一项目

---
+++ src/script/js/funtion.js
@@ -0,0 +1,8 @@
+// 导航编写
+
+// console.log($('.nav_a div a'));
+
+// $($('.nav_a div a')[0]).css({
+//     color: 'red',
+// });
+$('.nav_a div a').css("color", "red");
---
+++ src/script/js/home .js
@@ -0,0 +1,11 @@
+define(['jquery'], function() {
+    return {
+        teb: (function() {
+            alert(1);
+        })(),
+
+        lbt: function() {
+            alert(2);
+        }(),
+    }
+});
